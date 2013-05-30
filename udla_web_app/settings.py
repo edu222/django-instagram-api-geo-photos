3726201c416cc68f07f1e3ae6d08b62ac09fffa0
Eduardo Calvachi <educalvachi@gmail.com>
2013-05-29 22:51:17 -0500

    modified settings

--- udla_web_app/settings.py
+++ udla_web_app/settings.py
@@ -186,4 +186,7 @@ LOGGING = {
     }
 }
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SOUTH_TESTS_MIGRATE = False
